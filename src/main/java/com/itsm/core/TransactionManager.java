d464580dfd65ad23e934127c60b2e752c7e700fa
alexesmet <alexes.met@gmail.com>
2018-09-25 22:03:24 +0300

    Revert "Revert "add transaction support" for hot fix" oh my god what am i doing
    This reverts commit 416aa91a8d84e568a10ebc23a3c2b1d1f8c86d40.

---
+++ src/main/java/com/itsm/core/TransactionManager.java
@@ -0,0 +1,39 @@
+package com.itsm.core;
+
+import com.itsm.entity.Transaction;
+import com.itsm.factory.TransactionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TransactionManager {
+    private final String url;
+    private final TransactionFactory transactionFactory;
+
+    public TransactionManager(String url, TransactionFactory transactionFactory) {
+        this.url = url;
+        this.transactionFactory = transactionFactory;
+    }
+
+    void transact() throws SQLException {
+        Transaction transaction = transactionFactory.create(0);
+        if(transaction != null) {
+            if (transaction.getPatient().getState().getId() == transaction.getProduct().getState().getId()) {
+                Connection connection = DriverManager.getConnection(url);
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO transactions (client_id,product_id,time) VALUES (?, ?, ?)");
+                ps.setLong(1, transaction.getPatient().getId());
+                ps.setLong(2, transaction.getProduct().getId());
+                ps.setDate(3, transaction.getDatetime());
+                ps.execute();
+                ps.close();
+                connection.close();
+            } else {
+                throw new SQLException("Client's and Product's ID should be equal!");
+            }
+        } else {
+            System.out.println("Abort.");
+        }
+    }
+}
