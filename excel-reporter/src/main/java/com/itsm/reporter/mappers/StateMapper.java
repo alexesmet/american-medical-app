e06629ad35e924c42828b949af8e826733ee1c19
alexesmet <alexes.met@gmail.com>
2018-10-02 21:27:46 +0300

    added new mappers

--- excel-reporter/src/main/java/com/itsm/reporter/mappers/StateMapper.java
+++ excel-reporter/src/main/java/com/itsm/reporter/mappers/StateMapper.java
@@ -1,17 +1,10 @@
 package com.itsm.reporter.mappers;
 
 import com.itsm.common.entity.State;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 public interface StateMapper {
     @Select("SELECT * FROM states WHERE id=#{id}")
-    @Results({
-            @Result(column = "id", property = "id", javaType = long.class),
-            @Result(column = "name", property = "name", javaType = String.class),
-            @Result(column = "code", property = "code", javaType = int.class)
-    })
     State get(long id);
 
 }
--- excel-reporter/src/main/java/com/itsm/reporter/mappers/TransactionMapper.java
+++ excel-reporter/src/main/java/com/itsm/reporter/mappers/TransactionMapper.java
@@ -3,11 +3,9 @@ package com.itsm.reporter.mappers;
 import com.itsm.common.entity.Client;
 import com.itsm.common.entity.Product;
 import com.itsm.common.entity.Transaction;
-import org.apache.ibatis.annotations.One;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
+import java.util.Date;
 import java.util.List;
 
 public interface TransactionMapper {
@@ -16,11 +14,21 @@ public interface TransactionMapper {
             @Result(column = "id", property = "id", id = true),
             @Result(column = "time", javaType = java.util.Date.class, property = "datetime"),
             @Result(column = "client_id", javaType = Client.class, property = "patient",
-                    one = @One()), //TODO: Write a mapper
-            @Result(column = "product_id", javaType = Product.class, property = "product"),
-
+                    one = @One(select = "com.itsm.reporter.mappers.ClientMapper.get")),
+            @Result(column = "product_id", javaType = Product.class, property = "product",
+                    one = @One(select = "com.itsm.reporter.mappers.ProductMapper.get")),
     })
     List<Transaction> getAll();
 
-    //TODO: Add getBetweenDates()
+
+    @Select("SELECT * FROM transactions WHERE time BETWEEN #{dateBegin} AND #{dateEnd}")
+    @Results({
+            @Result(column = "id", property = "id", id = true),
+            @Result(column = "time", javaType = java.util.Date.class, property = "datetime"),
+            @Result(column = "client_id", javaType = Client.class, property = "patient",
+                    one = @One(select = "com.itsm.reporter.mappers.ClientMapper.get")),
+            @Result(column = "product_id", javaType = Product.class, property = "product",
+                    one = @One(select = "com.itsm.reporter.mappers.ProductMapper.get")),
+    })
+    List<Transaction> getAllInRange(@Param("dateBegin") Date dateBegin, @Param("dateEnd") Date dateEnd);
 }
